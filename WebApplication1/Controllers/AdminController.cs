9d72b7a5802caa1126b0edc4f6edc8ef717f656b
Bartlomiej.Mojek <Bartlomiej.Mojek@NY-19>
2023-02-04 10:51:37 +0100

    Dodaj pliki projektów.

---
+++ WebApplication1/Controllers/AdminController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication1.Controllers
+{
+    public class AdminController : Controller
+    {
+        [Authorize]
+        public IActionResult Index()
+        {
+            ViewBag.Mess = "to jest strona dla zalogwanych";
+            return View();
+        }
+        [AllowAnonymous]
+        public IActionResult Gosc()
+        {
+            ViewBag.Mess = "to jest strona dla wszystkich";
+            return View();
+        }
+        
+    }
+}
